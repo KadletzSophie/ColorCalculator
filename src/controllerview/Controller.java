04293b76fcdf326237855843186935d01085ec35
Sophie Kadletz <sophie.kadletz@htl-wels.at>
2021-01-22 12:08:30 +0100

    Model and Controller implemented

--- src/controllerview/Controller.java
+++ src/controllerview/Controller.java
@@ -4,8 +4,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.ColorCode;
+import model.Model_color;
+
+/**
+ * @author Sophie Kadletz
+ * @version 21.01.2021
+ */
 
 public class Controller {
+
+    private Model_color mc;
 
     @FXML
     private TextField blue_value;
@@ -22,49 +31,78 @@ public class Controller {
     @FXML
     private Label hex_value;
 
+
     @FXML
     void addTenBlue(ActionEvent event) {
-
+        mc.changeColorViaRelativeValue(ColorCode.BLUE,10);
+        blue_value.setText(Integer.toString(mc.getBlue()));
+        updateHex();
     }
 
     @FXML
     void addTenGreen(ActionEvent event) {
-
+        mc.changeColorViaRelativeValue(ColorCode.GREEN,10);
+        green_value.setText(Integer.toString(mc.getGreen()));
+        updateHex();
     }
 
     @FXML
     void addTenRed(ActionEvent event) {
-
+        mc.changeColorViaRelativeValue(ColorCode.RED,10);
+        red_value.setText(Integer.toString(mc.getRed()));
+        updateHex();
     }
 
     @FXML
     void blueAbsoluteValue(ActionEvent event) {
-
+        mc.changeColorViaAbsoluteValue(ColorCode.BLUE, blue_value.getText());
+        blue_value.setText(Integer.toString(mc.getBlue()));
+        updateHex();
     }
 
     @FXML
     void greenAbsoluteValue(ActionEvent event) {
-
+        mc.changeColorViaAbsoluteValue(ColorCode.GREEN, green_value.getText());
+        green_value.setText(Integer.toString(mc.getGreen()));
+        updateHex();
     }
 
     @FXML
     void redAbsoluteValue(ActionEvent event) {
-
+        mc.changeColorViaAbsoluteValue(ColorCode.RED, red_value.getText());
+        red_value.setText(Integer.toString(mc.getRed()));
+        updateHex();
     }
 
     @FXML
     void subTenBlue(ActionEvent event) {
-
+        mc.changeColorViaRelativeValue(ColorCode.BLUE,-10);
+        blue_value.setText(Integer.toString(mc.getBlue()));
+        updateHex();
     }
 
     @FXML
     void subTenRed(ActionEvent event) {
-
+        mc.changeColorViaRelativeValue(ColorCode.RED,-10);
+        red_value.setText(Integer.toString(mc.getRed()));
+        updateHex();
     }
 
     @FXML
     void sudTenGreen(ActionEvent event) {
+        mc.changeColorViaRelativeValue(ColorCode.GREEN,-10);
+        green_value.setText(Integer.toString(mc.getGreen()));
+        updateHex();
+    }
 
+    private void updateHex(){
+        hex_value.setText(mc.getHex());
+        color.setStyle("-fx-background-color:"+mc.getHex()+";");
+    }
+
+    @FXML
+    public void initialize(){
+        mc = new Model_color();
     }
 
 }
--- src/model/Model_color.java
+++ src/model/Model_color.java
@@ -15,28 +15,78 @@ public class Model_color {
     private String hex;
 
     public static void main(String[] args) {
-
+        
     }
 
     public Model_color() {
-
+        red = new ModularCounter(256);
+        green = new ModularCounter(256);
+        blue = new ModularCounter(256);
     }
 
     public void changeColorViaAbsoluteValue(ColorCode cc, String value) {
+        int intValue = Integer.parseInt(value);
 
+        switch (cc) {
+            case RED:
+                updateModularCounterAbsolute(intValue, red);
+                break;
+
+            case GREEN:
+                updateModularCounterAbsolute(intValue, green);
+                break;
+
+            case BLUE:
+                updateModularCounterAbsolute(intValue, blue);
+                break;
+        }
     }
 
     public void changeColorViaAbsoluteValue(ColorCode cc, int value) {
+        switch (cc) {
+            case RED:
+                updateModularCounterAbsolute(value, red);
+                break;
+
+            case GREEN:
+                updateModularCounterAbsolute(value, green);
+                break;
+
+            case BLUE:
+                updateModularCounterAbsolute(value, blue);
+                break;
+        }
+    }
+
+    private void updateModularCounterAbsolute(int intValue, ModularCounter mc) {
+        mc.reset();
+        mc.update(intValue);
+    }
+
+    public void changeColorViaRelativeValue(ColorCode cc, String value) {
+        int intValue = Integer.parseInt(value);
+        updateModularCounterRelative(cc, intValue);
 
     }
 
+    private void updateModularCounterRelative(ColorCode cc, int value) {
+        switch (cc) {
+            case RED:
+                red.update(value);
+                break;
 
-    public void changeColorViaRelativeValue(ColorCode cc, String value) {
+            case GREEN:
+                green.update(value);
+                break;
 
+            case BLUE:
+                blue.update(value);
+                break;
+        }
     }
 
     public void changeColorViaRelativeValue(ColorCode cc, int value) {
-
+        updateModularCounterRelative(cc, value);
     }
 
     public int getRed() {
@@ -52,7 +102,8 @@ public class Model_color {
     }
 
     public String getHex() {
-       return hex;
+        hex = String.format("#%02x%02x%02x", red.getValue(), green.getValue(), blue.getValue()).toUpperCase();
+        return hex;
     }
 
     @Override
